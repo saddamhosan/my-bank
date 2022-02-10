8de6115419ae6844cf3c08190566ff54e79ea3bf
saddamhosan <contact.saddamhosan@gmail.com>
2022-02-10 11:54:03 +0600

    finished

---
+++ js/banking.js
@@ -0,0 +1,24 @@
+const depositBtn = document.getElementById("deposit-btn");
+depositBtn.addEventListener("click", function () {
+  const depositField = document.getElementById("deposit-field");
+  const depositAmount = parseFloat(depositField.value);
+  const depositBox = document.getElementById("deposit");
+  const deposit = parseFloat(depositBox.innerText);
+  depositBox.innerText = deposit + depositAmount;
+  depositField.value = "";
+  const balanceBox = document.getElementById("balance");
+  const balance = parseFloat(balanceBox.innerText);
+  balanceBox.innerText = balance + depositAmount;
+});
+const withdrawBtn = document.getElementById("withdraw-btn");
+withdrawBtn.addEventListener("click", function () {
+  const withdrawField = document.getElementById("withdraw-field");
+  const withdrawAmount = parseFloat(withdrawField.value);
+  const withdrawBox = document.getElementById("withdraw");
+  const withdraw = parseFloat(withdrawBox.innerText);
+  withdrawBox.innerText = withdraw + withdrawAmount;
+  withdrawField.value = "";
+  const balanceBox = document.getElementById("balance");
+  const balance = parseFloat(balanceBox.innerText);
+  balanceBox.innerText = balance - withdrawAmount;
+});
